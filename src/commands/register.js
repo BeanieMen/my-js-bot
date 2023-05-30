51896a3b550cd830252fe2ed231d8254d357f8e5
me505 <Jainarjav886@gmail.com>
2023-05-30 15:40:28 +0530

    master

--- src/commands/register.js
+++
@@ -1,7 +0,0 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-
-const registerCommand = new SlashCommandBuilder()
-  .setName('register')
-  .setDescription('Register a user to the server officially');
-
-export default registerCommand.toJSON();
